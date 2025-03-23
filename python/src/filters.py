abee97c82950c0fccb302dc98018468e3212f46f
Minh Le <minhhlee1711@gmail.com>
2025-03-23 14:33:09 -0700

    Adding comments to filters.py

--- python/src/filters.py
+++ python/src/filters.py
@@ -1,8 +1,10 @@
-from datetime import datetime
+from datetime import datetime # import datetime module for date handling
 
+# filter expenses with a certain category
 def filter_by_category(expenses, category):
     return [exp for exp in expenses if exp.category.lower() == category.lower()]
 
+# filter by start and end date (inclusive)
 def filter_by_date_range(expenses, start_date, end_date):
     start = datetime.strptime(start_date, "%Y-%m-%d")
     end = datetime.strptime(end_date, "%Y-%m-%d")
