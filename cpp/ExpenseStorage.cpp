4b55f7d943fb5550d4b88690547662f6ce713eb9
asrith4444 <asrithvejandla@gmail.com>
2025-03-23 17:57:12 -0400

    Added a clear documentation for cpp application

--- cpp/ExpenseStorage.cpp
+++ cpp/ExpenseStorage.cpp
@@ -1,10 +1,19 @@
 // ExpenseStorage.cpp
+// This file provides functionality for saving and loading expense data.
+// It defines the functions that handle reading and writing Expense objects
+// to a file, using a delimiter-based format.
+
 #include "ExpenseStorage.h"
 #include <fstream>
 #include <sstream>
 #include <iostream>
 #include <vector>
 
+/**
+ * Saves a list of expenses to a specified file.
+ * @param expenses A vector containing Expense objects to be saved.
+ * @param filename The name of the file where expenses will be stored.
+ */
 void saveExpenses(const std::vector<Expense>& expenses, const std::string& filename) {
     std::ofstream file(filename);
     if (!file) {
@@ -17,6 +26,11 @@ void saveExpenses(const std::vector<Expense>& expenses, const std::string& filen
     }
 }
 
+/**
+ * Loads a list of expenses from a specified file.
+ * @param expenses A reference to a vector where loaded Expense objects will be stored.
+ * @param filename The name of the file from which expenses will be read.
+ */
 void loadExpenses(std::vector<Expense>& expenses, const std::string& filename) {
     std::ifstream file(filename);
     if (!file) {
