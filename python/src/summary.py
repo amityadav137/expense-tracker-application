0ef35c5af8d9d8aa8fd38c79e52808f9450d3ca2
Minh Le <minhhlee1711@gmail.com>
2025-03-23 14:31:56 -0700

    Add comments to summary.py

--- python/src/summary.py
+++ python/src/summary.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 
+# sum all expenses function
 def total_expenses(expenses):
     return sum(exp.amount for exp in expenses)
 
+# sum expenses by category using dictionary
 def expenses_by_category(expenses):
     category_totals = defaultdict(float)
     for exp in expenses:
